ba9a1cca5328430ce28549b85afb6dedef28c448
yaswanthdakkatha123 <yaswantdakkatha123@gmail.com>
2026-09-05 14:08:03 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -10,3 +10,37 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(fetch(event.request));
 });
+self.addEventListener("install", event => {
+    console.log("Service Worker installed");
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
+});
+
+// Notification click
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({
+            type: "window",
+            includeUncontrolled: true
+        }).then(clientList => {
+            for (const client of clientList) {
+                if ("focus" in client) {
+                    return client.focus();
+                }
+            }
+
+            if (clients.openWindow) {
+                return clients.openWindow("/");
+            }
+        })
+    );
+});
