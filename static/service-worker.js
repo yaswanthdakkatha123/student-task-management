9d71c9431459b1942b17a60c27538218e1cd9483
yaswanthdakkatha123 <yaswantdakkatha123@gmail.com>
2026-09-05 14:04:02 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -10,3 +10,11 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(fetch(event.request));
 });
+
+<script>
+if ("Notification" in window) {
+    Notification.requestPermission().then(function(permission) {
+        console.log("Notification permission:", permission);
+    });
+}
+</script>
