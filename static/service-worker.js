911a3304a62e7d8054526d10cb26e03e812c20de
yaswanthdakkatha123 <yaswantdakkatha123@gmail.com>
2026-09-05 13:55:39 +0530

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker installed");
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
+});
